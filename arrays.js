2ad5f9f7f295f58801861b8bc4eb4b68ba3273f5
Abdullah Bajaman <ambajaman@gmail.com>
2019-06-10 11:56:13 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -166,6 +166,6 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 */
 function removeElementFromEndOfArray(array){
    array = array.slice(0 , array.length -1);
-  // return array
+  return array
 
 }
