8dcc895da468cd7d947414478bb1eaf80704d435
Abdullah Bajaman <ambajaman@gmail.com>
2019-06-10 10:10:03 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,49 @@
+var chocolateBars= ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+/*************************************************************************************
+ *  describe('addElementToBeginningOfArray(array, element)', () => {
+    it('adds an element to the beginning of an array', () => {
+      expect(addElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1])
+    })
+
+    it('does not alter the original array', () => {
+      const array = [1]
+
+      addElementToBeginningOfArray(array, 'foo')
+
+      expect(array).to.eql([1])
+    })
+  })
+  */
+function addElementToBeginningOfArray(array, element){
+  element = "foo"
+  array =[element, ...array]
+ 
+  // moreArray= [element, ...array]
+  // array[1]= element;
+  return array
+  
+}
+
+/*************************************************************************************
+ * describe('destructivelyAddElementToBeginningOfArray(array, element)', () => {
+    it('adds an element to the beginning of an array', () => {
+      expect(destructivelyAddElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1])
+    })
+
+    it('alters the original array', () => {
+      const array = [1]
+
+      destructivelyAddElementToBeginningOfArray(array, 'foo')
+
+      expect(array).to.eql(['foo', 1])
+    })
+  })
+*/
+function destructivelyAddElementToBeginningOfArray(array, element){
+  element = "foo"
+  array =[element, ...array]
+  moreArray= [element, ...array]
+
+  return moreArray
+}
