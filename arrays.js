254f1982dacbb59be5256ec1afa48b67136ccc42
Abdullah Bajaman <ambajaman@gmail.com>
2019-06-10 11:35:33 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -134,4 +134,18 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   }
   
   /*************************************************************************************
-   * 
+   * describe('destructivelyRemoveElementFromEndOfArray(array)', () => {
+    it('returns the array with the last element removed', () => {
+      expect(destructivelyRemoveElementFromEndOfArray([1, 2, 3])).to.eql([1, 2])
+    })
+    
+    it('alters the original array', ()=>{
+      const array = [1, 2, 3];
+      destructivelyRemoveElementFromEndOfArray(array);
+      expect(array).to.eql([1, 2]);
+    })
+  })
+  */
+  function destructivelyRemoveElementFromEndOfArray(array){
+    array = array.pop()
+  }
